bcddd6bcb4192ea89e65ba5d3f884baaa5bf04d4
Matt Jones <m.t.h.jones@gmail.com>
2012-10-18 17:41:15 -0600

    Add validation to check end date > start date for Workshop

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -4,4 +4,12 @@ class Workshop < ActiveRecord::Base
   validates :title, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  validate :validate_end_date_later_than_start_date
+
+  def validate_end_date_later_than_start_date
+  	if self.end_date && self.start_date && (self.start_date > self.end_date)
+  		self.errors.add(:end_date, "must be a later date than the start date.")
+  	end
+  end
 end
