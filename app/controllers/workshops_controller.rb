f0bc036dda0ed53fc430a6b782df550b112c48ec
Matt Jones <m.t.h.jones@gmail.com>
2012-10-20 09:44:19 -0600

    Update non-GET methods in Workshops controller to prevent non-ursers from using them

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -16,12 +16,16 @@ class WorkshopsController < ApplicationController
 	end
 
 	def create
-		@workshop = Workshop.new(params[:workshop])
-		if @workshop.save
-			flash[:success] = "Workshop successfully created!"
-			redirect_to @workshop
+		if current_user
+			@workshop = Workshop.new(params[:workshop])
+			if @workshop.save
+				flash[:success] = "Workshop successfully created!"
+				redirect_to @workshop
+			else
+				render 'new'
+			end
 		else
-			render 'new'
+			redirect_to status: 404
 		end
 	end
 
@@ -34,18 +38,26 @@ class WorkshopsController < ApplicationController
 	end
 
 	def update
-		@workshop = Workshop.find(params[:id])
-		if @workshop.update_attributes(params[:workshop])
-			flash[:success] = "Workshop successfully updated!"
-			redirect_to @workshop
+		if current_user
+			@workshop = Workshop.find(params[:id])
+			if @workshop.update_attributes(params[:workshop])
+				flash[:success] = "Workshop successfully updated!"
+				redirect_to @workshop
+			else
+				render 'edit'
+			end
 		else
-			render 'edit'
+			redirect_to status: 404
 		end
 	end
 
 	def destroy
-		Workshop.find(params[:id]).destroy
-		flash[:success] = "Workshop successfully deleted!"
-		redirect_to workshops_url
+		if current_user
+			Workshop.find(params[:id]).destroy
+			flash[:success] = "Workshop successfully deleted!"
+			redirect_to workshops_url
+		else
+			redirect_to new_session_path
+		end
 	end
 end
