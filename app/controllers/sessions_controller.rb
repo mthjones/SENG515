a7f3e63c317a68f1baa5801ad90724740fae79d3
Matt Jones <m.t.h.jones@gmail.com>
2012-10-23 13:30:10 -0600

    Set up method stubs for Session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+  def index
+    
+  end
+  
+  def show
+    @session = Session.find(params[:id])
+  end
+  
+  def new
+    @session = Session.new
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
 end
