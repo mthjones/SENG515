efc1aa618ecfae4212d299b3f5b5604eadc5d8e5
Matt Jones <m.t.h.jones@gmail.com>
2012-10-23 13:25:42 -0600

    Update Session migration to use datetime instead of date

--- db/migrate/20121018182536_create_workshops.rb
+++ db/migrate/20121018182536_create_workshops.rb
@@ -3,8 +3,8 @@ class CreateWorkshops < ActiveRecord::Migration
     create_table :workshops do |t|
       t.string :title
       t.text :description
-      t.date :start_date
-      t.date :end_date
+      t.datetime :start_date
+      t.datetime :end_date
 
       t.timestamps
     end
