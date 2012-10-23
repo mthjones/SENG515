dad5bedd8479bd01c14ef9045c868fb1ae023419
Matt Jones <m.t.h.jones@gmail.com>
2012-10-23 13:27:20 -0600

    Set up Sessions controller stubs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,2 @@
 class SessionsController < ApplicationController
-  def new
-  end
-  
-  def create
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      flash[:success] = "Logged in!"
-      #redirect_to workshops_path
-      redirect_to(session[:return_to] || default)
-    else
-      flash.now.alert = "Invalid email or password"
-      render "new"
-    end
-  end
-  
-  def destroy
-    session[:user_id] = nil
-    redirect_to(:back, :notice => "Logged out!") 
-  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
-describe UserSessionsController do
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
+describe SessionsController do
 
 end
