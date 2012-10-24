58520711c6a5f3658b0e1ab97f8dd42a62a3ce78
Matt Jones <m.t.h.jones@gmail.com>
2012-10-24 01:10:17 -0600

    Add validations to Session

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,4 +2,16 @@ class Session < ActiveRecord::Base
   attr_accessible :description, :end_datetime, :start_datetime, :title
   
   belongs_to :workshop
+  
+  validates :title, presence: true
+  validates :start_datetime, presence: true
+  validates :end_datetime, presence: true
+
+  validate :validate_end_datetime_later_than_start_datetime
+
+  def validate_end_datetime_later_than_start_datetime
+    if self.end_datetime && self.start_datetime && (self.start_datetime > self.end_datetime)
+      self.errors.add(:end_datetime, "must be a later date than the start date.")
+    end
+  end
 end
