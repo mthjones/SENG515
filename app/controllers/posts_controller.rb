7308095a965c0eb5f20895add86fda9fbc6d8f28
shawniut <Shawn@.(none)>
2012-10-24 17:19:13 -0600

    Showing individual post and updating post is done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,35 @@ class PostsController < ApplicationController
       session[:return_to] = request.url
   end
   def show
-      @post = Posts.find(params[:id])
+      @post = Post.find(params[:id])
       session[:return_to] = request.url
   end
   def new
+  end
+  def create
+    
+  end
+  def edit
+    if current_user_is_admin
+      @post = Post.find(params[:id])
+    else
+      redirect_to new_user_session_path
+    end 
+  end
+  def update
+    if current_user_is_admin
+      @post = Post.find(params[:id])
+      if @post.update_attributes(params[:post])
+        flash[:success] = "Workshop successfully updated!"
+        redirect_to @post
+      else
+        render 'edit'
+      end
+    else
+      redirect_to status: 404
+    end
+  end
+  def destroy
+    
   end
 end
