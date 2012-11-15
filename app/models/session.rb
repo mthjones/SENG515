d3ff19acf3665969e5a8141bb297e8646a188114
Matt Jones <m.t.h.jones@gmail.com>
2012-11-14 17:54:41 -0700

    Add validation for daterange room overlaps

--- app/models/session.rb
+++ app/models/session.rb
@@ -9,11 +9,29 @@ class Session < ActiveRecord::Base
   validates :start_datetime, presence: true
   validates :end_datetime, presence: true
 
-  validate :validate_end_datetime_later_than_start_datetime
+  validate :end_datetime_later_than_start_datetime
+  validate :room_not_double_booked
 
-  def validate_end_datetime_later_than_start_datetime
+  def end_datetime_later_than_start_datetime
     if self.end_datetime && self.start_datetime && (self.start_datetime > self.end_datetime)
       self.errors.add(:end_datetime, "must be a later date than the start date.")
     end
+  end
+  
+  def room_not_double_booked
+    self.room.sessions.each do |sess|
+      unless sess == self
+        if self.overlaps_times_with(sess)
+          self.errors.add(:room_id, "is already booked on the selected dates. (" + sess.title + ")")
+          break
+        end
+      end
+    end
+  end
+  
+  protected
+  
+  def overlaps_times_with(other)
+    (self.start_datetime..self.end_datetime).overlaps?(other.start_datetime..other.end_datetime)
   end
 end
