14829b8041657cd98171b3b7bcaeb2edf47dc8d1
Orest Nazarewycz <Orest.Nazarewycz@gmail.com>
2012-11-14 21:39:37 -0700

    Seeded Database
    Seeded the database for equipment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
 Workshop.delete_all
 (1..100).each do |num|
 	Workshop.create!(title: (0...15).map{ (('a'..'z').to_a + ('A'..'Z').to_a + [' ']*10).to_a[rand(62)] }.join, description: (0...200).map{ (('a'..'z').to_a + ('A'..'Z').to_a + [' ']*10).to_a[rand(62)] }.join, start_date: Date.today, end_date: Date.today + 21.days)
 end
+
+Equipment.delete_all
+	Equipment.create!(name: "DVD Player", 		  description: "Standard DVD player")
+	Equipment.create!(name: "Projector",  		  description: "Projector to be used with a computer")
+	Equipment.create!(name: "TV", 		  		  description: "Standard Television")
+	Equipment.create!(name: "Whiteboard", 		  description: "Classroom whiteboard")
+	Equipment.create!(name: "Blackboard", 		  description: "Classroom blackboard")
+	Equipment.create!(name: "Overhead Projector", description: "Overhead Projector for displaying slides")
+
+
+
