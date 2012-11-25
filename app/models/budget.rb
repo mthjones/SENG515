8009fb3350ed9806e4649fb8878ed22dda288ed4
Matt Jones <m.t.h.jones@gmail.com>
2012-11-25 15:31:20 -0700

    Budget can now accept expenses nested attributes

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,4 +2,6 @@ class Budget < ActiveRecord::Base
   belongs_to :workshop
   has_many :expenses, dependent: :destroy
   attr_accessible :accomodation_cost, :venue_cost
+  
+  accepts_nested_attributes_for :expenses
 end
