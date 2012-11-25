ef24333484a9c23ccbf601e9d1adab01a06c6624
Matt Jones <m.t.h.jones@gmail.com>
2012-11-25 16:04:33 -0700

    Can now add expense inputs to form

--- app/assets/javascripts/budgets.js.coffee
+++ app/assets/javascripts/budgets.js.coffee
@@ -1,3 +1,8 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+@add_expense = (link, association, content) ->
+  new_id = new Date().getTime()
+  regexp = new RegExp("new_" + association, "g")
+  $(link).parent().before(content.replace(regexp, new_id))
--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -1,2 +1,9 @@
 module BudgetsHelper
+  def link_to_add_expense(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, "add_expense(this, \"#{association}\", \"#{j(fields)}\")")
+  end
 end
