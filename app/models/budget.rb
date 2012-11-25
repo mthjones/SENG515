8e053aba320f3f22cd97af60e39b57f6df73cf0f
Matt Jones <m.t.h.jones@gmail.com>
2012-11-25 16:08:16 -0700

    Can now destroy expenses

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -3,5 +3,5 @@ class Budget < ActiveRecord::Base
   has_many :expenses, dependent: :destroy
   attr_accessible :accomodation_cost, :venue_cost
   
-  accepts_nested_attributes_for :expenses
+  accepts_nested_attributes_for :expenses, :allow_destroy => true
 end
