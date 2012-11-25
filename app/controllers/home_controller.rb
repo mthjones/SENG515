715fa2dccd0ed6ce76eb10305d2e41788ca00682
Matt Jones <m.t.h.jones@gmail.com>
2012-11-25 01:10:26 -0700

    Homepage shows recent posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @workshops = Workshop.where("start_date >= ?", Date.today).order("start_date ASC").limit(5)
+    @posts = Post.order("created_at DESC").limit(5)
   end
 end
