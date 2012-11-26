0a0f9e375471be404fdf06bd4b3d4c5ad3c0c19f
Matt Jones <m.t.h.jones@gmail.com>
2012-11-25 17:17:44 -0700

    Add validations to expenses and check on update

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -6,8 +6,12 @@ class BudgetsController < ApplicationController
   
   def update
     @workshop = Workshop.find(params[:workshop_id])
-    @workshop.budget.update_attributes(params[:budget])
-    flash[:notice] = "Budget updated successfully."
-    redirect_to edit_workshop_budget_path(@workshop)
+    @budget = @workshop.budget
+    if @workshop.budget.update_attributes(params[:budget])
+      flash[:notice] = "Budget updated successfully."
+      redirect_to edit_workshop_budget_path(@workshop)
+    else
+      render 'edit'
+    end
   end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,7 @@
 class Expense < ActiveRecord::Base
   belongs_to :budget
   attr_accessible :amount, :name
+  
+  validates :name, presence: true
+  validates :amount, presence: true
 end
