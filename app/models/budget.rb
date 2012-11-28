51cb99a1fe95282f143dfe5e2eecea5b09c78fc3
Matt Jones <m.t.h.jones@gmail.com>
2012-11-27 21:20:56 -0700

    Budget now shows total

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -4,4 +4,8 @@ class Budget < ActiveRecord::Base
   attr_accessible :accomodation_cost, :venue_cost, :expenses_attributes
   
   accepts_nested_attributes_for :expenses, :allow_destroy => true
+  
+  def total
+    return accomodation_cost + venue_cost + expenses.map(&:amount).sum
+  end
 end
