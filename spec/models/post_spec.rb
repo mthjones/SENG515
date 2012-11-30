191e395c71dfa45dd593d5ad6968edd7d8ece349
Orest Nazarewycz <Orest.Nazarewycz@gmail.com>
2012-11-29 23:02:25 -0700

    Removed test files for Helper functions

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+	 before (:each) do 
+   		@post = Post.new(title: 'post test title',
+   						 content: 'post test content',
+   						 admin_only: true)
+ end
+
+ it "verify post needs a title to be valid" do
+ 	@post.title = ''
+ 	@post.valid?.should == false
+ end
+
+
+ it "verify post cannot have non alphanumeric title" do
+ 	@post.title = '#{)()()()()}'
+ 	@post.valid?.should
+ end
+
+ it "verify post needs content" do
+ 	@post.content = ''
+ 	@post.valid?.should == false
+ end 
+
+ it "verify post should be valid with bost title and content" do
+ 	@post.title = 'asd'
+ 	@post.content = 'asda'
+ 	@post.valid?
+ end
+
 end
