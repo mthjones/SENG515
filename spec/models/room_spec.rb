066da4f6fd230f4a3a93aaad076a41a0c5661302
Orest Nazarewycz <Orest.Nazarewycz@gmail.com>
2012-11-29 21:45:42 -0700

    Added Room model tests

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Room do
-  pending "add some examples to (or delete) #{__FILE__}"
+	 before (:each) do 
+   		@room = Room.new(description: 'test@test.com',
+   						 room_no: 'qweasd',
+   						 location: 'building A')
+  
+ end
+
+ it "room requires a room number" do
+ 	@room.room_no = ''
+ 	@room.valid?.should == false
+ end
+
+ it "room requries a location" do
+ 	@room.location = ''
+ 	@room.valid?.should  == false
+ end
+
+ it "room requires both room and location" do
+ 	@room.valid?.should == true
+ end
+
+
 end
