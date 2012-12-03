f4e5289a6e3c1793cfc537c375895c1b8dc611bc
Matt Jones <m.t.h.jones@gmail.com>
2012-12-03 13:35:09 -0700

    Updated seeds file to properly delete budgets

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Room.delete_all
+Budget.delete_all
 Workshop.delete_all
 Session.delete_all
 Room.delete_all
