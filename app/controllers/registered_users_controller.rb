19f61b69924ce35a90624b6c38884bbd01f96e02
Orest Nazarewycz <Orest.Nazarewycz@gmail.com>
2012-12-02 17:47:54 -0700

    Registered_users page now shows registered users

--- app/controllers/registered_users_controller.rb
+++ app/controllers/registered_users_controller.rb
@@ -1,16 +1,33 @@
 class RegisteredUsersController < ApplicationController
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
+   before_filter :authenticate_admin!
+  
   def index
+    @users = User.all
+  end
+  
+  def show
+    @users = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+   # @users = Users.find(params[:id])
+    #if @users.update_attributes(params[:email])
+      #flash[:success] = "Equipment successfully saved!"
+     # redirect_to admin_registered_users_index_path
+    #else
+     # render :edit
+   # end
+  end
+  
+  def destroy
+    Users.find(params[:id]).destroy
+    flash[:success] = "User successfully deleted!"
+    redirect_to admin_registered_users_index_path
   end
 
-  def show
-  end
 end
+
