0e7ff3472f30e41818c845fa19896068a4b4f471
Matt Jones <m.t.h.jones@gmail.com>
2012-12-05 12:19:46 -0700

    Removed verified from attr_accessible on user model to prevent mass assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :verified
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   
   has_and_belongs_to_many :sessions
   has_many :workshops, :through => :sessions, :uniq => true
