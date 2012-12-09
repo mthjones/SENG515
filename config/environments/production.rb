4d9e6c89f9ed185bc205ddbcfe3faeaba750add2
Matt Jones <m.t.h.jones@gmail.com>
2012-12-09 13:50:09 -0700

    Updated host url for production to match app rename

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ SENG515::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'hidden-chamber-1598.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'seng515.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
